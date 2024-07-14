86ea21cde10c5b4d210f069c09f5f3d8ab48562a
Michael Bumann <hello@michaelbumann.com>
2024-07-15 00:33:53 +0200

    Enable logs for debugging and notifications

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -9,6 +9,7 @@ export LND_CERT_FILE="/mnt/lnd/tls.cert"    #the location where LND's tls.cert f
 export LND_MACAROON_FILE="/mnt/lnd/admin.macaroon" #the location where LND's admin.macaroon file can be found (used with the LND backend)
 export WORK_DIR="/data/albyhub"
 export PORT=8080 #the port on which the app should listen on (default='blah' #8080)
+export LOG_EVENTS=true # makes debugging easier
 
 export TOR_ADDRESS=$(yq e '.tor-address' /data/start9/config.yaml)
 export LAN_ADDRESS=$(yq e '.lan-address' /data/start9/config.yaml)
