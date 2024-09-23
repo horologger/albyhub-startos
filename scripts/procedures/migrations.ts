d8889a2fa8f828c10ac5ec61f8fc1ec293a82bd6
Mariusz Kogen <k0gen@pm.me>
2024-09-23 20:57:14 +0200

    Update to the latest 1.9.0

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "1.8.0" );
+    .fromMapping({}, "1.9.0" );
